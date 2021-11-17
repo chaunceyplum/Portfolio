4f1c27a5ae93d1ce07367907ec27e34ece578f82
Chauncey <Chaunceyplum@gmail.com>
2021-11-17 03:11:47 -0500

    fixing favicon and Name of site

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Footer from './components/Footer';
-import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 import Home from './components/Home'
 import Lobby from './components/Lobby';
 import ParticleBG from './components/ParticleBG';
+
 
 library.add(fab, faCheckSquare, faCoffee)
 
@@ -39,17 +40,19 @@ export default class App extends Component {
     return (
       <div>
           
-            <BrowserRouter>
+            {/* <HashRouter basename="/">
               <Mynav />
                 <Switch > 
                   
-                  <Route path ="/home" component={Homepage} />
-                  <Route path ="/lobby" component={Lobbypage} />
-                  <Redirect to="/lobby" />
-                </Switch>
+                  <Route path ="/home" render={props => <Home {...props} />} />
+                  <Route path ="/lobby" render={props => <Lobby {...props} />}  />
+                  {/* <Redirect to="/lobby" /> */}
+                {/* </Switch>
               <Footer  className="fixed-bottom"/>
-            </BrowserRouter> 
-          
+            </HashRouter>  */}
+          <Mynav />
+          <Home />
+          <Footer />
         
       </div>
 
