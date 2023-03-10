e0d62071abb40e16b1f8dc79f0f36a9f17ebf1c2
Chauncey <Chaunceyplum@gmail.com>
2023-03-10 11:02:15 -0500

    added new project before overhaul

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
-import './CSS/styles.css';
+import './CSS/styles.css'
 import Mynav from './components/Nav'
 import React from 'react'
-import Footer from './components/Footer';
-import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Footer'
+import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
-import Lobby from './components/Lobby';
+import Lobby from './components/Lobby'
 import Resume from './components/Resume'
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import NotFound from './components/NotFound';
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import NotFound from './components/NotFound'
 
 const App = () => {
-  
-  
-  
   return (
-    <div>
+    <div className='bg'>
       <Mynav />
-      <TransitionGroup>
-        <CSSTransition key={Location.key} classNames="page" timeout={300}>
-          <Routes > 
+      {/* <TransitionGroup>
+        <CSSTransition key={Location.key} classNames='page' timeout={300}> */}
+      <Routes>
+        <Route exact path='/' element={<Lobby />} />
+        <Route exact path='/home' element={<Home />} />
+        <Route exact path='/resume' element={<Resume />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+      {/* </CSSTransition>
+      </TransitionGroup> */}
 
-            <Route exact path ="/" element={<Lobby />}  />
-            <Route exact path="/home" element={<Home />} />
-            <Route exact path="/resume" element={<Resume />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </CSSTransition>
-      </TransitionGroup>
-          
-
-      <Footer  className="fixed-bottom"/>
+      <Footer className='fixed-bottom' />
     </div>
   )
 }
 
 export default App
-
-
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,141 +1,163 @@
 import React, { useState } from 'react'
-import { Button, Card, CardBody, CardHeader, CardImg, Col, Container, Row } from 'reactstrap'
-import {projects} from './projects'
-import { FaGithub, FaInstagram, FaLinkedin, FaTwitter, FaReact, FaHtml5, FaCss3, FaNodeJs, FaJs, FaBootstrap } from 'react-icons/fa'
-import 'bootstrap/dist/css/bootstrap.css';
-
-
-
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  CardImg,
+  Col,
+  Container,
+  Row,
+} from 'reactstrap'
+import { projects } from './projects'
+import {
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaReact,
+  FaHtml5,
+  FaCss3,
+  FaNodeJs,
+  FaJs,
+  FaBootstrap,
+} from 'react-icons/fa'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const Home = () => {
-    const Projects = useState(projects)
-    
-    
-    
-    
-    return (
-        <div className="bg">
-            <Container className="bg spacer">
-                <Row className="bg space-evenly">
-                    <Col className="bg1 " xs={12} lg={3}>
-                        <h3 className=" spacer" >
-                            Chauncey Plummer
-                        </h3>
-                        <h6 className="spacer">
-                            Front End Web Developer
-                        </h6>
-                        <img />
+  const Projects = useState(projects)
 
-                        <h6 className="spacer">
-                            About Me
-                        </h6>
-                        <p>
-                        An infectiously enthusiastic Front End Web Developent Bootcamp graduate with fundamental knowledge of web development. Seeking to utilize advanced expertise in javaScript
-                        </p>
-                        <h6 className="spacer">
-                            Technologies I use
-                        </h6>
-                        <ul className="unstyle spacer ">
-                            <li className="unstyle spacer "><FaReact /> React</li>
-                            <li className="unstyle spacer "><FaNodeJs /> Node Js</li>
-                            <li className="unstyle spacer "><FaBootstrap />Bootstrap</li>
-                        </ul>
-                        
-                        <Row className="centerFooter justify-content-space-evenly spacer" >
-                        
-                            {/* <Col className="d-flex justify-content-center spacer">
+  return (
+    <div className='bg'>
+      <Container className='bg spacer'>
+        <Row className='bg space-evenly'>
+          <Col className='bg1 ' xs={12} lg={3}>
+            <h3 className=' spacer'>Chauncey Plummer</h3>
+            <h6 className='spacer'>Front End Web Developer</h6>
+            <img />
+
+            <h6 className='spacer'>About Me</h6>
+            <p>
+              An infectiously enthusiastic Front End Web Developent Bootcamp
+              graduate with fundamental knowledge of web development. Seeking to
+              utilize advanced expertise in javaScript
+            </p>
+            <h6 className='spacer'>Technologies I use</h6>
+            <ul className='unstyle spacer '>
+              <li className='unstyle spacer '>
+                <FaReact /> React
+              </li>
+              <li className='unstyle spacer '>
+                <FaNodeJs /> Node Js
+              </li>
+              <li className='unstyle spacer '>
+                <FaBootstrap />
+                Bootstrap
+              </li>
+            </ul>
+
+            <Row className='centerFooter justify-content-space-evenly spacer'>
+              {/* <Col className="d-flex justify-content-center spacer">
                                 <Button href='https://www.instagram.com/chaveloper/' className='buttons' size="20em">
                                     <FaInstagram />
                                 </Button>
                             </Col> */}
-                            <Col className="d-flex justify-content-center spacer">
-                                <Button href='https://www.linkedin.com/in/chauncey-plummer-5457361bb/' className='buttons' size="20em">
-                                    <FaLinkedin />
-                                </Button>
-                            </Col>
-                            <Col className="d-flex justify-content-center spacer">
-                                <Button href='https://twitter.com/chaunce_dev' className='buttons'  size="20em">
-                                    <FaTwitter />
-                                </Button>
-                            </Col>
-                            <Col className="d-flex justify-content-center spacer">
-                                
-                                <Button href='https://github.com/chaunceyplum'className='buttons'  >
-                                    <FaGithub />
-                                </Button>
-                            </Col>
-                    
+              <Col className='d-flex justify-content-center spacer'>
+                <Button
+                  target='_blank'
+                  rel='noopener'
+                  href='https://www.linkedin.com/in/chauncey-plummer-5457361bb/'
+                  className='buttons'
+                  size='20em'
+                >
+                  <FaLinkedin />
+                </Button>
+              </Col>
+              <Col className='d-flex justify-content-center spacer'>
+                <Button
+                  target='_blank'
+                  rel='noopener'
+                  href='https://twitter.com/chaunce_dev'
+                  className='buttons'
+                  size='20em'
+                >
+                  <FaTwitter />
+                </Button>
+              </Col>
+              <Col className='d-flex justify-content-center spacer'>
+                <Button
+                  target='_blank'
+                  rel='noopener'
+                  href='https://github.com/chaunceyplum'
+                  className='buttons'
+                >
+                  <FaGithub />
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+          <Col xs={1} />
+          <Col className='bg1 ' xs={12} lg={8}>
+            <h3 className='spacer'>My Projects</h3>
+            <div
+              className='col-10 justify-content-center mx-auto space-evenly rounded'
+              md={8}
+              xs={12}
+            >
+              {projects.map((project, index) => {
+                return (
+                  <div className='' key={index}>
+                    <Card className=''>
+                      <CardHeader className='cardHeader'>
+                        <h3 className='spacer'>{project.name}</h3>
+
+                        <CardImg src={project.image} />
+                      </CardHeader>
+
+                      <CardBody className='cardBg'>
+                        <p>{`Project Stack: ${project.stack}`}</p>
+                        <p>{`Technologies Used: ${project.technologies}`}</p>
+                        <p>{project.description}</p>
+
+                        <Row>
+                          <Col>
+                            <Button
+                              href={project.githubLink}
+                              className='buttons'
+                              target='_blank'
+                              rel='noopener'
+                            >
+                              Link to Github
+                            </Button>
+                          </Col>
+                          <Col>
+                            <Button
+                              href={project.projectLink}
+                              className='buttons'
+                              target='_blank'
+                              rel='noopener'
+                            >
+                              Link to Project
+                            </Button>
+                          </Col>
                         </Row>
-
-
-                        
-
-                    </Col>
-                    <Col xs={1}/>
-                    <Col className="bg1 " xs={12} lg={8}>
-                        <h3 className="spacer">
-                            My Projects
-                        </h3>
-                        <div className="col-10 justify-content-center mx-auto space-evenly rounded" md={8} xs={12}>
-                            {
-                                projects.map((project, index) => {
-                                    return(
-                                        <div className=""  key={index}>
-                                            <Card className="">
-                                                <CardHeader className="cardHeader">
-                                                    <h3 className="spacer">
-                                                        {project.name}
-                                                    </h3>
-
-                                                    <CardImg src={project.image}  />
-                                                </CardHeader>
-                                                
-                                                <CardBody className="cardBg">
-                                                    <p>
-                                                        {`Project Stack: ${project.stack}`}
-                                                    </p>
-                                                    <p>
-                                                        {
-                                                            `Technologies Used: ${project.technologies}`
-                                                        }
-                                                    </p>
-                                                    <p>
-                                                        {
-                                                            project.description
-                                                        }
-                                                    </p>
-                                                    
-                                                    <Row>
-                                                        <Col>
-                                                            <Button href={project.githubLink} className="buttons">
-                                                                Link to Github
-                                                            </Button>
-                                                        </Col>
-                                                        <Col>
-                                                            <Button href={project.projectLink} className="buttons">
-                                                                Link to Project
-                                                            </Button>
-                                                        </Col>
-                                                        
-                                                    </Row>
-                                                </CardBody>
-                                            </Card>
-                                            <br />
-                                            <br />
-                                            <br />
-                                            <br />
-                                            <br />
-                                            <br />
-                                        </div>
-                                    )
-                                })
-                            }
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
+                      </CardBody>
+                    </Card>
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                  </div>
+                )
+              })}
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  )
 }
 
 export default Home
